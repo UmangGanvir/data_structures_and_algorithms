96202d3c4a3ea2994b883ca8697f0faa20cee87e
Umang Ganvir <umangganvir93@gmail.com>
2017-05-06 19:11:05 +0530

    zig_zag_level_order_traversal

---
+++ problems/trees/zig_zag_level_order_traversal.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void insertNodeAtLevel( map<int,vector<TreeNode*>>& levelNodeMap, TreeNode* A, int level ){
+    if( A == NULL )
+        return;
+
+    auto it = levelNodeMap.find( level );
+    if( it != levelNodeMap.end() ){
+        it->second.push_back( A );
+    } else {
+        vector<TreeNode*> arr( 1, A );
+        pair<int, vector<TreeNode*>> pair1( level, arr );
+        levelNodeMap.insert( pair1 );
+    }
+
+    insertNodeAtLevel( levelNodeMap, A->left, level+1 );
+    insertNodeAtLevel( levelNodeMap, A->right, level+1 );
+}
+
+vector<vector<int> > zigzagLevelOrder(TreeNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    map<int,vector<TreeNode*>> levelNodeMap;
+    insertNodeAtLevel( levelNodeMap, A, 0 );
+
+    vector<vector<int>> ans;
+    for( auto it = levelNodeMap.begin(); it != levelNodeMap.end(); it++ ){
+        vector<int> ansOfLevel( it->second.size() );
+        for( int i=0; i<it->second.size(); i++ ){
+            ansOfLevel[i] = it->second[i]->val;
+        }
+        if( it->first % 2 == 1 ){
+            reverse( ansOfLevel.begin(), ansOfLevel.end() );
+        }
+        ans.push_back( ansOfLevel );
+    }
+    return ans;
+}
+
+int main() {
+
+    //
+    return 0;
+}
