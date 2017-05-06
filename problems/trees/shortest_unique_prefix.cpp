2f5860a2f030d7c9694d8ea3383f8ecab7efe67a
Umang Ganvir <umangganvir93@gmail.com>
2017-05-06 18:44:00 +0530

    shortest_unique_prefix

---
+++ problems/trees/shortest_unique_prefix.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void printStringVector( vector<string> vec ){
+    cout<<"Printing string vector: "<<endl;
+    int n = (int) vec.size();
+    for( int i=0; i<n; i++ )
+        cout<<vec[i]<<" ";
+    cout<<endl;
+}
+
+class Trie {
+public:
+    vector<int> wordCounts;
+    vector<Trie*> arr;
+    Trie(){
+        vector<int> wordsArr(26, 0);
+        this->wordCounts = wordsArr;
+        vector<Trie*> arr1(26, NULL);
+        this->arr = arr1;
+    }
+};
+
+void insertIntoTrie( string str, Trie* root ){
+
+    Trie* trav = root;
+
+    for( int i=0; i<str.length(); i++ ){
+
+        Trie* nextTrieNode = trav->arr[ (int)str[i] - 97 ];
+        if( nextTrieNode == NULL )
+            nextTrieNode = new Trie();
+
+        trav->arr[ (int)str[i] - 97 ] = nextTrieNode;
+        trav->wordCounts[ (int)str[i] - 97 ]++;
+        trav = nextTrieNode;
+    }
+}
+
+string fetchMinimalStringFromTrie( string str, Trie* root ){
+
+    string ans = "";
+    Trie* trav = root;
+    for( int i=0; i<str.length(); i++ ){
+        ans += string(1, str[i]);
+        if( trav->wordCounts[ (int)str[i] - 97 ] == 1 )
+            break;
+        trav = trav->arr[ (int)str[i] - 97 ];
+    }
+    return ans;
+
+}
+vector<string> prefix(vector<string> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    Trie* root = new Trie();
+
+    // Form the dictionary
+    for( int i=0; i<A.size(); i++ ){
+        insertIntoTrie( A[i], root );
+    }
+
+    // Find shortest prefix for representation
+    vector<string> ans( A.size() );
+    for( int i=0; i<A.size(); i++ ){
+        ans[i] = fetchMinimalStringFromTrie( A[i], root );
+    }
+
+    return ans;
+}
+
+
+int main() {
+
+    vector<string> A = { "zebra", "dog", "duck", "dove" };
+    printStringVector( prefix( A ) );
+    return 0;
+}
