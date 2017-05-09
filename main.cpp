fdf746c29eae9f80f05b221cc84cdb1be4eabf67
Umang Ganvir <umangganvir93@gmail.com>
2017-05-10 01:15:41 +0530

    temporarily changing template before moving to next section

--- main.cpp
+++ main.cpp
@@ -3,14 +3,6 @@
 #include <vector>
 using namespace std;
 
-class TreeNode {
-public:
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 int methodName( string A ){
     return 123;
 }
